eca279954c6a88c563f4a4333138ba46a20755e4
lee <castillolee@yahoo.com>
2018-09-24 10:10:19 +0800

    latest commit

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,37 +1,61 @@
 import React, {PropTypes} from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../actions/courseAction';
+import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props.context);
-        this.state = {
-          course: {title: ""}  
-        };
+        // this.state = {
+        //   course: {title: ""}  
+        // };
 
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClcikSave.bind(this);
+        // this.onTitleChange = this.onTitleChange.bind(this);
+        // this.onClickSave = this.onClcikSave.bind(this);
     }
 
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
+    // onTitleChange(event) {
+    //     const course = this.state.course;
+    //     course.title = event.target.value;
+    //     this.setState({course: course});
+    // }
 
-    onClcikSave() {
-        alert(`Saving ${this.state.course.title}`);
+    // onClcikSave() {
+    //     this.props.actions.createCourse(this.state.course);
+    // }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>
     }
 
     render() {
+        const {courses} = this.props;
         return(
             <div>
                 <h1>Courses</h1>
-                <h2>Add course</h2>
-                <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
-
-                <input type="submit" value="Save" onClick={this.onClickSave} />
+                {/* {this.props.courses.map(this.courseRow)} */}
+                <CourseList courses={courses} />
             </div>
         );
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        courses: state.courses
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import routes from './routes';
+import {loadCourses} from './actions/courseAction';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+store.dispatch(loadCourses());
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
